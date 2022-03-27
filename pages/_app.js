65d15a3157b8325f1f2443c517e9fa5ff0b160c7
lazySia <tldk9640@naver.com>
2022-03-27 22:45:50 +0900

    getServerSideProps
    API key 백엔드에서 처리하기

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,10 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: tomato;
-        }
-      `}</style>
     </Layout>
   );
 }
