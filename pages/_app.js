d0dd906c05620d93d7d0fd44b59142f5078aadb9
lazySia <tldk9640@naver.com>
2022-03-27 13:45:43 +0900

    patterns
    layout, head

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
       <style jsx global>{`
         a {
-          color: white;
+          color: tomato;
         }
       `}</style>
-    </>
+    </Layout>
   );
 }
