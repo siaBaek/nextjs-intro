04726b7597a7abebdeb95824ad8030d149c8b99c
lazySia <tldk9640@naver.com>
2022-03-28 01:48:02 +0900

    detail page url SSR

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,7 @@ import Seo from "../components/Seo";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          id,
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -27,15 +18,7 @@ export default function Home({ results }) {
           key={movie.id}
         >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <Link
-            href={{
-              pathname: `/movies/${movie.id}`,
-              query: {
-                title: movie.original_title,
-              },
-            }}
-            as={`/movies/${movie.id}`}
-          >
+          <Link href={`/movies/${movie.original_title}/${movie.id}`}>
             <a>
               <h4>{movie.original_title}</h4>
             </a>
--- pages/movies/[id].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Detail() {
-  const router = useRouter();
-  console.log(router);
-
-  return <h4>{router.query.title || "loading"}</h4>;
-}
